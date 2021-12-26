775e5b16b118d926aecded7b6ee543933a428e7e
unknown <okwuosahpashal@gmail.com>
2021-12-26 01:03:11 +0100

    first commit

---
+++ src/requestMethods.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = "https://e-com1-backend.herokuapp.com/api/";
+const TOKEN =
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+    .accessToken || "";
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
