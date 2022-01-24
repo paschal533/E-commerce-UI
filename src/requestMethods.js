8fec301aed6c7ca225bddd46109d4b0a22075b08
unknown <okwuosahpaschal@gmail.com>
2022-01-24 23:28:39 +0100

    second commit

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -9,7 +9,4 @@ export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
-export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
-});
+ 
